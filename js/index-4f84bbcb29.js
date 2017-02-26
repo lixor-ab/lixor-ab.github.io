d2fd199433c3aab754e81a258c90b35319ea3dc0
Travis CI <ulf@alfhild.io>
2017-02-26 00:47:10 +0000

    Deploy to GitHub Pages: 5721148c8d8c019bc7ec90db91edd38b92624ae0

---
+++ js/index-4f84bbcb29.js
@@ -0,0 +1,1 @@
+"use strict";(function(document){var sections=Array.prototype.slice.call(document.querySelectorAll(".section-summary"));sections.forEach(function(el){el.addEventListener("click",onToggle,false)});function onToggle(e){e.currentTarget.parentNode.classList.toggle("expanded")}})(document);
